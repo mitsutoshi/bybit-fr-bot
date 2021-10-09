a57836203274c0fe616c5d302c9ab78fa57ce6b3
mitsutoshi <mitsu3204@gmail.com>
2021-10-10 01:17:38 +0900

    Fixed run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -5,13 +5,13 @@ TZ=Asia/Tokyo
 IMAGE_NAME=bybit-fr-bot
 CONTAINER_NAME=$IMAGE_NAME
 
-docker stop frbot > /dev/null 2>&1
+docker stop $CONTAINER_NAME > /dev/null 2>&1
 
-docker rm frbot > /dev/null 2>&1
+docker rm $CONTAINER_NAME > /dev/null 2>&1
 
 docker run \
   -d \
   -v $LOG_DIR:/home/root/logs \
   -e TZ=$TZ \
   --name $CONTAINER_NAME \
-  $IMAGER_NAME:latest
+  $IMAGE_NAME:latest
